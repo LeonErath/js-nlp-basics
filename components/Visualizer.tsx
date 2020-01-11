f408a31643cc778612a871fd22ff11101d3c6a08
Leon Erath <leon-erath@hotmail.de>
2020-01-11 13:52:08 +0100

    :bug: trying to resolve compromise bug

--- components/Visualizer.tsx
+++ components/Visualizer.tsx
@@ -30,18 +30,18 @@ const Visualizer = () => {
 			const { text } = node;
 			const terms = (nlp(text)
 				.terms()
-				.out("offsets") as unknown) as Array<any>;
-			terms.forEach((term: any) => {
-				ranges = [
-					...ranges,
-					{
-						anchor: { path, offset: term.offset.start },
-						focus: { path, offset: term.offset.start + term.offset.length },
-						highlight: true,
-						tag: term.terms[0].tags[0]
-					}
-				];
-			});
+				.out("tags") as unknown) as Array<any>;
+			// terms.forEach((term: any) => {
+			// 	ranges = [
+			// 		...ranges,
+			// 		{
+			// 			anchor: { path, offset: term.offset.start },
+			// 			focus: { path, offset: term.offset.start + term.offset.length },
+			// 			highlight: true,
+			// 			tag: term.terms[0].tags[0]
+			// 		}
+			// 	];
+			// });
 		}
 
 		return ranges;
