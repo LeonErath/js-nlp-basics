6828fd30d9b6704878f8ca699f6d0617c52d4fd2
Leon Erath <leon-erath@hotmail.de>
2021-01-09 12:32:30 +0100

    feat: Adds file upload capabilities

--- next.config.js
+++ next.config.js
@@ -4,6 +4,10 @@ require("dotenv").config();
 const withCSS = require("@zeit/next-css");
 module.exports = withCSS({
 	env: {
-		MONGODB_URI: process.env.MONGODB_URI
-	}
+		MONGODB_URI: process.env.MONGODB_URI,
+		ACCESS_KEY: process.env.ACCESS_KEY,
+		SECRET_KEY: process.env.SECRET_KEY,
+		REGION: process.env.REGION,
+		BUCKET_NAME: process.env.BUCKET_NAME,
+	},
 });
