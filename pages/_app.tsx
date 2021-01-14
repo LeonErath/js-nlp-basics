c8e56a571cbc5dcab219e56d58c60173bb40588a
Leon Erath <leon-erath@hotmail.de>
2021-01-14 21:06:25 +0100

    feat: Adds page loading spinner

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
 import 'antd/dist/antd.css';
 import React from 'react';
 import Page from '../components/Page';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
+import { Router } from 'next/router';
+import '../styles/index.css';
+
+NProgress.configure({ showSpinner: false });
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 const MyApp = ({ Component, pageProps }) => {
 	return (
